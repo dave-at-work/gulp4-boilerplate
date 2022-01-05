c30b2ffd1bef4c4fe55185216f4a81dbf6506dee
= <=>
2022-01-05 09:05:19 +0100

    typo fix

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ const jsImport = require('gulp-js-import');
 const sourcemaps = require('gulp-sourcemaps');
 const htmlPartial = require('gulp-html-partial');
 const clean = require('gulp-clean');
-const gulpHtmlPartial = require('gulp-html-partial');
+const htmlPartial = require('gulp-html-partial');
 const isProd = process.env.NODE_ENV === 'prod';
 
 const htmlFiles = [
